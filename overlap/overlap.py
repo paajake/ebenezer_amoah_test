aeb84713323f4cb8f8cf2106751a984915bd80fc
Paa J A K E <paajake@gmail.com>
2019-12-16 15:44:38 +0000

    Lint Cleanup

--- overlap/overlap.py
+++ overlap/overlap.py
@@ -3,34 +3,36 @@ from typing import Union, Tuple
 Coordinate = Union[int, float]
 
 
-def is_overlaping(line_1: Tuple[Coordinate, Coordinate], line_2: Tuple[Coordinate, Coordinate]) -> bool:
-    """ 
+def is_overlapping(line_1: Tuple[Coordinate, Coordinate],
+                   line_2: Tuple[Coordinate, Coordinate]) -> bool:
+    """
     A line overlap comparator.
-    This function takes in the start and end coordinates of 2 lines in the x-line plane as tuples, and checks if they overlap. 
-  
-    Parameters: 
-    line_1 ( Tuple(int|float, int|float) ): This is a Tuple of 2 numbers representing the 2 x coordinates that form this line, 
-                                            assumes the first coordinate is lower than the second.
-    line_2 ( Tuple(int|float, int|float) ): This is a Tuple of 2 numbers representing the 2 x coordinates that form this line 
-                                            assumes the first coordinate is lower than the second.
+    This function takes in the start and end coordinates of 2 lines in the x-line plane as tuples, and checks if they
+    overlap.
+
+    Parameters : 
+    line_1 ( Tuple(int|float, int|float) ): This is a Tuple of 2 numbers representing the 2 x coordinates that form this
+                                            line,assumes the first coordinate is lower than the second.
+    line_2 ( Tuple(int|float, int|float) ): This is a Tuple of 2 numbers representing the 2 x coordinates that form this
+                                            line assumes the first coordinate is lower than the second.
 
     Returns: 
     bool: Returns True if lines overlap, else it returns False.
 
     Examples:
-    >>> is_overlaping((0,9),(1,8))
+    >>> is_overlapping((0,9),(1,8))
     True
-    >>> is_overlaping((0,9),(0,8))
+    >>> is_overlapping((0,9),(0,8))
     True
-    >>> is_overlaping((0,5),(7,15))
+    >>> is_overlapping((0,5),(7,15))
     False
     """
-    
-    #For Lines to overlap, the end point of the line with lower boundary, needs to exceed the start point of the other.
-    
+
+    # For Lines to overlap, the end point of the line with lower boundary, needs to exceed the start point of the other.
+
     lower_bounded_line: Tuple
     higher_bounded_line: Tuple
-    
+
     if line_1[0] <= line_2[0]:
         lower_bounded_line = line_1
         higher_bounded_line = line_2
@@ -38,6 +40,7 @@ def is_overlaping(line_1: Tuple[Coordinate, Coordinate], line_2: Tuple[Coordinat
         lower_bounded_line = line_2
         higher_bounded_line = line_1
 
-    if (lower_bounded_line[1] > higher_bounded_line[0] ): return True
+    if lower_bounded_line[1] > higher_bounded_line[0]:
+        return True
 
     return False
--- overlap/test_overlap.py
+++ overlap/test_overlap.py
@@ -1,21 +1,23 @@
 import unittest
-from overlap import is_overlaping
+from overlap import is_overlapping
+
 
 class TestOverlap(unittest.TestCase):
     def test_lines_overlap(self):
-        self.assertEqual( is_overlaping ((0, 10), (2, 15)), True)
-        self.assertEqual( is_overlaping ((10, 25), (5, 16)), True)
-        self.assertEqual( is_overlaping ((20, 25), (10, 30)), True)
-    
+        self.assertEqual(is_overlapping((0, 10), (2, 15)), True)
+        self.assertEqual(is_overlapping((10, 25), (5, 16)), True)
+        self.assertEqual(is_overlapping((20, 25), (10, 30)), True)
+
     def test_lines_dont_overlap(self):
-        self.assertEqual( is_overlaping ((0, 10), (10.1, 15)), False)
-        self.assertEqual( is_overlaping ((-12, -5), (0, 10)), False)
-        self.assertEqual( is_overlaping ((-10, 0.0), (0.0, 10)), False)
-    
+        self.assertEqual(is_overlapping((0, 10), (10.1, 15)), False)
+        self.assertEqual(is_overlapping((-12, -5), (0, 10)), False)
+        self.assertEqual(is_overlapping((-10, 0.0), (0.0, 10)), False)
+
     def test_lines_types(self):
-       with self.assertRaises(TypeError): is_overlaping((9,15), "overlap")
-       with self.assertRaises(TypeError): is_overlaping("text", (12,21))
-       with self.assertRaises(TypeError): is_overlaping((9,15), ("12",20))
+        with self.assertRaises(TypeError): is_overlapping((9, 15), "overlap")
+        with self.assertRaises(TypeError): is_overlapping("text", (12, 21))
+        with self.assertRaises(TypeError): is_overlapping((9, 15), ("12", 20))
+
 
 if __name__ == '__main__':
     unittest.main()
