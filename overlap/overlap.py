daa415ad8cff29e89108d6a05e99a3bae9a70fff
Paa J A K E <paajake@gmail.com>
2019-12-16 13:56:06 +0000

    Added overlap Function

---
+++ overlap/overlap.py
@@ -0,0 +1,33 @@
+from typing import Union, Tuple
+
+Num = Union[int, float]
+
+
+def is_overlaping(line_1: Tuple[Num, Num], line_2: Tuple[Num, Num]) -> bool:
+    """ 
+    A line overlap comparator.
+    This function takes in the start and end coordinates of 2 lines in the x-line plane as tuples, and checks if they overlap. 
+  
+    Parameters: 
+    line_1 ( Tuple(int|float, int|float) ): This is a Tuple of 2 numbers representing the 2 x coordinates that form this line.
+    line_2 ( Tuple(int|float, int|float) ): This is a Tuple of 2 numbers representing the 2 x coordinates that form this line.
+
+    Returns: 
+    bool: Returns True if lines overlap, else it returns False.
+    """
+    
+    #For Lines to overlap, the end point of the line with lower boundary, needs to exceed the start point of the other.
+    
+    lower_bounded_line: Tuple
+    higher_bounded_line: Tuple
+    
+    if line_1[0] <= line_2[0]:
+        lower_bounded_line = line_1
+        higher_bounded_line = line_2
+    else:
+        lower_bounded_line = line_2
+        higher_bounded_line = line_1
+
+    if (lower_bounded_line[1] > higher_bounded_line[0] ): return True
+
+    return False
