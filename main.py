47a3585a96b49eda196c08c568bb73146f17fa8d
Paa J A K E <paajake@gmail.com>
2019-12-17 15:45:56 +0000

    Added overlap test to main script

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-def prompt_options() -> int:
-    print("ORMUCO Technical Test\nSelect a Test by entering it's corresponding\
- integer :\n" + "*"*55)
+from overlap.overlap import is_overlapping
 
-    print("1.  Lines Overlap Test(Question A)\n\
-2.  Versions Comparisons Test (Question B)\n\
-9.  Exit\n\nSelect option : ")
+
+def prompt_options() -> int:
+    print("ORMUCO Technical Test\nSelect a Test by entering it's corresponding"
+          + " integer :\n" + "*"*55)
+
+    print("1.  Lines Overlap Test (Question A)\n"
+          + "2.  Versions Comparisons Test (Question B)\n"
+          + "9.  Exit\n\nSelect option : ")
 
     try:
         return int(input().strip())
@@ -16,14 +19,47 @@ def prompt_options() -> int:
 
 
 def error_exit(error_count: int):
-    print(f"Too many errors generated of count {error_count}\n \
-App will now Exit")
+    print(f"\n\nToo many errors generated of count {error_count}\n"
+          + "App will now Exit")
+    exit()
+
+
+def success_exit():
+    print(f"\n\nThanks for testing the Modules\n{'*'*20} B Y E ! ! ! {'*'*20}")
     exit()
 
 
 def lines_overlap(error_count: int):
+    if(error_count > 2):
+        error_exit(error_count)
 
-    return error_count
+    line_1 = list()
+    line_2 = list()
+
+    print("Lines Overlap :\n" + "*"*50)
+
+    try:
+        for i in range(0, 2):
+            line_1.append(float(input(f"Enter the cordinate {i+1} of Line 1: ")))
+
+        for i in range(0, 2):
+            line_2.append(float(input(f"Enter the cordinate {i+1} of Line 2: ")))
+    except ValueError:
+        print("ERROR : A coordinate MUST be a Number\n")
+        lines_overlap(error_count + 1)
+
+    overlap = is_overlapping(tuple(line_1), tuple(line_2))
+
+    if overlap is True:
+        print(f"\nResults:\n Yes, Line 1 ({line_1[0]},{line_1[1]}) and "
+              + f"Line 2 ({line_2[0]},{line_2[1]}) indeed Do Overlap\n\n")
+        return main(0)
+    elif overlap is False:
+        print(f"\nResults:\nNo, Line 1 ({line_1[0]},{line_1[1]}) and "
+              + f"Line 2 ({line_2[0]},{line_2[1]}) DO NOT Overlap\n\n")
+        return main(0)
+
+    return main(1)
 
 
 def main(error_count: int):
@@ -36,10 +72,13 @@ def main(error_count: int):
     valid_choices = [1, 2, 9]
 
     if choice in valid_choices:
-        return 1
+        if choice == 1:
+            lines_overlap(0)
+        elif choice == 9:
+            success_exit()
     else:
         error_count += 1
-        print("Invalid Choice Try Again...")
+        print("\nInvalid Choice Try Again...")
         main(error_count)
 
 
--- versions_comparison/test_versions_comparison.py
+++ versions_comparison/test_versions_comparison.py
@@ -3,17 +3,17 @@ from versions_comparison import compare
 
 
 class TestVersionsCompare(unittest.TestCase):
-    def test_vesrsion_1_greater(self):
+    def test_version_1_greater(self):
         self.assertEqual(compare("1", "0.0"), 1)
         self.assertEqual(compare("1", "-10"), 1)
         self.assertEqual(compare("10.2", "5.5.5"), 1)
 
-    def test_vesrsions_equal(self):
+    def test_versions_equal(self):
         self.assertEqual(compare("1", "1"), 0)
         self.assertEqual(compare("0.0.1", "0.0.1"), 0)
         self.assertEqual(compare("3.4", "3.4.0"), 0)
 
-    def test_vesrsion_1_lesser(self):
+    def test_version_1_lesser(self):
         self.assertEqual(compare("0.0", "1"), -1)
         self.assertEqual(compare("0.0.1", "0.0.5"), -1)
         self.assertEqual(compare("3.4", "3.4.1"), -1)
