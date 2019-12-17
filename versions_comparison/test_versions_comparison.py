17bc47841e312d2c0f85153969e4aaa7a87b5af2
Paa J A K E <paajake@gmail.com>
2019-12-17 09:28:18 +0000

    Added Version compare Test of types

--- versions_comparison/test_versions_comparison.py
+++ versions_comparison/test_versions_comparison.py
@@ -18,6 +18,16 @@ class TestVersionsCompare(unittest.TestCase):
         self.assertEqual(compare("0.0.1", "0.0.5"), -1)
         self.assertEqual(compare("3.4", "3.4.1"), -1)
 
+    def test_args_types(self):
+        with self.assertRaises(TypeError):
+            compare("0.0")
+        with self.assertRaises(TypeError):
+            compare("0.1", "0.2", "0.3")
+        with self.assertRaises(AttributeError):
+            compare(1, "0.0")
+        with self.assertRaises(ValueError):
+            compare("0.1", "0.a")
+
 
 if __name__ == '__main__':
     unittest.main()
